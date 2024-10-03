4630cf24716e89de76aa40ef5640d9e0343eeb17
0jun <bill5500@naver.com>
2024-10-03 11:12:41 +0900

    .

---
+++ src/array/array5_2.java
@@ -0,0 +1,31 @@
+package src.array;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class array5_2 {
+    public static int solution(int n) {
+        int answer=0;
+        int[] arr = new int[n+1]; //자동으로 0으로 초기화됨
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            if (arr[i] == 0) {
+                answer += 1;
+                for (int j = i; j < n+1; j=j+i) {
+                    arr[j] = 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        array5_2 T = new array5_2();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
+
