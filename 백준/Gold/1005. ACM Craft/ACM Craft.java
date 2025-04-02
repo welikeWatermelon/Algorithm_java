daf5b1e260662cfdf54188e53888a008b2431e18
welikeWatermelon <150949835+welikeWatermelon@users.noreply.github.com>
2025-04-02 09:45:25 +0900

    [Gold III] Title: ACM Craft, Time: 2124 ms, Memory: 311572 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -48,15 +48,9 @@ public class Main {
 //                minTime[start] += D[start]; // 건물 하나만 지을 때 걸리는 시간을 더해줘 (start 건물)
                 for (int c : graph.get(start)) {
                     inDegree[c]--;
-                    minTime[c] = Math.max(minTime[start]+D[c], minTime[c]);
-                    // Math.max(A, B); : A는 현재 전 건물에서 걸린 시간, B는 현재 건물까지 더한게 걸린 시간
-                    // 전에까지 더해서 갱신했던 minTime[c] 와 현재 이미 다른 건물 과정에서 걸린 시간인 minTime[c] 값 중
-                    // 큰 값을 거해줌
-                    // D[start]를 계속 갱신해줘야함
-                    // c의 위상차수가 2라고 가정해보자
-                    // 첫번 째, 위상차수가 1이 될 때, D[start1]이 minTime으로 들어올거고
-                    // 두번 째, 위상차수가 0이 될 때, D[start2]이 minTime으로 들어올건데, 이 때 최대 값으로 갱신해줘야한다.
+                    minTime[c] = Math.max(minTime[start], minTime[c]);
                     if (inDegree[c] == 0) {
+                        minTime[c] += D[c]; // 자기 시간 추가
                         q.add(c);
                     }
                 }
