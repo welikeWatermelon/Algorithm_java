e7cb376c1d6eaa59664d2430bd61cbd0824196cf
welikeWatermelon <150949835+welikeWatermelon@users.noreply.github.com>
2025-04-18 12:37:21 +0900

    [Silver II] Title: 최대 힙, Time: 1488 ms, Memory: 115824 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,23 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int tmp = sc.nextInt();
+            if (tmp != 0) {
+                pq.add(tmp);
+            } else {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
+}
