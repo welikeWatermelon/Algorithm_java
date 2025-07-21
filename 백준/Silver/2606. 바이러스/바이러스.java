70c101542d027867fefa2c73badd7577d64a5f15
welikeWatermelon <150949835+welikeWatermelon@users.noreply.github.com>
2025-07-21 23:56:14 +0900

    [Silver III] Title: 바이러스, Time: 192 ms, Memory: 18008 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,35 +1,59 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static int[][] arr;
-    public static boolean[] checked;
+
+    public static List<List<Integer>> graph = new ArrayList<>();
+    public static boolean[] visited;
     public static int N;
+    public static int M;
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
-        int M = sc.nextInt();
-        arr = new int[N + 1][N + 1];
-        checked = new boolean[N + 1];
-        for (int i = 0; i < M; i++) {
-            int first = sc.nextInt();
-            int second = sc.nextInt();
-            arr[first][second] = 1;
-            arr[second][first] = 1;
+        M = sc.nextInt();
+
+        visited = new boolean[N+1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>()); // 각 인덱스 안에 리스트 생성
         }
 
-        checked[1] = true;
-        System.out.println(DFS(1, 0));
-    }
 
-    public static int DFS(int idx,int cnt) {
-        int tmparr[] = arr[idx];
-        for (int i = 1; i < N + 1; i++) {
-            if (tmparr[i] == 1 && !checked[i]) {
-                checked[i] = true;
-                cnt = DFS(i, cnt + 1);
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        bfs(1);
+
+        int cnt = 0;
+
+        for (int i = 2; i <= N; i++) {
+            if (visited[i]) {
+                cnt++;
             }
         }
 
-        return cnt;
+        System.out.println(cnt);
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int next : graph.get(current)){
+                if (!visited[next]){
+                    visited[next]=true;
+                    queue.offer(next);
+                }
+            }
+        }
     }
 }
