e1803302f685aa6ecc1662bbc3625816de3ee62c
welikeWatermelon <bill19985500@gmail.com>
2025-12-30 15:28:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,61 +3,56 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    public static int N,cnt;
+
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
-    public static int[][] arr;
-    public static boolean[][] checked;
+    public static int N;
 
-    //체크드 하고, 계속 카운트 세줘야함
-    // 만약 카운트 다하고 만약 다음 단지로 이동할 경우 cnt = 0으로 초기화 시켜줘야함
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        arr = new int[N][N];
-        checked = new boolean[N][N];
-        ArrayList<Integer> answers = new ArrayList<>();
+        int[][] arr = new int[N][N];
+        boolean[][] check = new boolean[N][N];
+        int group = 0;
+        ArrayList<Integer> cntAnswer = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            String tmp = sc.next();
+            String line = sc.next();
             for (int j = 0; j < N; j++) {
-                arr[i][j] = tmp.charAt(j) - '0';
+                arr[i][j] = line.charAt(j) - '0';
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!checked[i][j] && arr[i][j] == 1) {
-                    checked[i][j] = true;
-                    cnt = 1;
-                    DFS(i, j);
-                    answers.add(cnt);
+                if (arr[i][j] == 1 && !check[i][j]) {
+                    cntAnswer.add(dfs(i, j, arr, check));
+                    group++;
                 }
             }
         }
 
-        int leng = answers.size();
-        System.out.println(leng);
-
-        if (leng != 0) {
-            Collections.sort(answers);
-            for (int i = 0; i < leng-1; i++) {
-                System.out.println(answers.get(i));
-            }
-            System.out.print(answers.get(leng-1));
+        // 출력
+        System.out.println(group);
+        Collections.sort(cntAnswer);
+        for (int i = 0; i < cntAnswer.size(); i++) {
+            System.out.println(cntAnswer.get(i));
         }
 
     }
 
-    public static void DFS(int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && arr[nx][ny] == 1 && !checked[nx][ny]) {
-                checked[nx][ny] = true;
-                cnt++;
-                DFS(nx, ny);
+    public static int dfs(int i, int j, int[][] arr, boolean[][] check){
+        check[i][j] = true;
+        int count = 1;
+
+        for (int k = 0; k < 4; k++) {
+            int nx = dx[k] + i;
+            int ny = dy[k] + j;
+            if (nx < N && ny < N && nx >= 0 && ny >= 0 && arr[nx][ny] == 1 && !check[nx][ny]) {
+                count+= dfs(nx, ny, arr, check);
             }
         }
+
+        return count;
     }
 }
