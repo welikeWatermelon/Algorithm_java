763b02694d1cfd7e3c5a325de3371abb60587449
welikeWatermelon <bill19985500@gmail.com>
2025-12-30 16:23:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 496 ms, Memory: 39536 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,73 +1,79 @@
 import java.util.*;
 
 public class Main {
-    public static int N, M, V;
-    public static ArrayList<ArrayList<Integer>> graph;
-    public static boolean[] visitedDFS;
-    public static boolean[] visitedBFS;
+
+    public static int N;
+    public static int M;
+    public static int V;
+    public static node[] arr;
+    public static boolean[] checked;
+
+
+    public static class node{
+        ArrayList<Integer> links = new ArrayList<>();
+
+        public void plusLink(int link) {
+            links.add(link);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        V = sc.nextInt();
 
-        N = sc.nextInt();  // 정점의 개수
-        M = sc.nextInt();  // 간선의 개수
-        V = sc.nextInt();  // 시작 정점
+        arr = new node[N+1];
+        checked = new boolean[N+1];
 
-        graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+            arr[i] = new node();
         }
 
-        // 간선 정보 입력
         for (int i = 0; i < M; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            arr[start].plusLink(end);
+            arr[end].plusLink(start);
         }
 
-        // 각 정점의 인접 리스트 정렬 (작은 번호 우선 방문)
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(graph.get(i));
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(arr[i].links);
         }
 
-        // DFS 탐색
-        visitedDFS = new boolean[N + 1];
-        DFS(V);
-        System.out.println();  // DFS 출력 후 줄바꿈
-
-        // BFS 탐색
-        visitedBFS = new boolean[N + 1];
-        BFS(V);
+        checked[V] = true;
+        System.out.print(V);
+        dfs(V);
+        System.out.println();
+        checked = new boolean[N + 1];
+        bfs(V);
     }
 
-    // DFS 구현 (재귀)
-    public static void DFS(int v) {
-        visitedDFS[v] = true;
-        System.out.print(v + " ");
-        for (int i = 0; i < graph.get(v).size(); i++) {
-            int next = graph.get(v).get(i);
-            if (!visitedDFS[next]) {
-                DFS(next);
+    public static void dfs(int V){
+        for (int nextNode : arr[V].links) {
+            if (!checked[nextNode]) {
+                checked[nextNode]  = true;
+                System.out.print(" " + nextNode);
+                dfs(nextNode);
             }
         }
+
     }
 
-    // BFS 구현 (큐 사용)
-    public static void BFS(int v) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(v);
-        visitedBFS[v] = true;
+    public static void bfs(int V){
+        System.out.print(V);
 
+        node now = arr[V];
+        checked[V] = true;
+        Queue<node> queue = new LinkedList<>();
+        queue.add(now);
         while (!queue.isEmpty()) {
-            int node = queue.poll();
-            System.out.print(node + " ");
-
-            for (int i = 0; i < graph.get(node).size(); i++) {
-                int next = graph.get(node).get(i);
-                if (!visitedBFS[next]) {
-                    visitedBFS[next] = true;
-                    queue.offer(next);
+            node nowNode = queue.poll();
+            for (int i : nowNode.links) {
+                if (!checked[i]) {
+                    checked[i] = true;
+                    queue.add(arr[i]);
+                    System.out.print(" " + i);
                 }
             }
         }
