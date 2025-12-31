e2c25d7dae8720a7e7ac0eab6faba3fc595d4357
welikeWatermelon <bill19985500@gmail.com>
2025-12-31 10:43:32 +0900

    [Silver I] Title: 미로 탐색, Time: 204 ms, Memory: 19044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,57 +1,53 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
+    public static int N;
+    public static int M;
+    public static int[][] arr;
+    public static boolean[][] visited;
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        sc.nextLine();
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-        int[][] arr = new int[N][M];
-        boolean[][] visited = new boolean[N][M]; // 방문 여부 체크
-
+        arr = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String line = sc.nextLine();
+            String line = sc.next();
             for (int j = 0; j < M; j++) {
                 arr[i][j] = line.charAt(j) - '0';
             }
         }
+        visited = new boolean[N][M];
 
-        System.out.println(bfs(N, M, arr));
+        bfs();
     }
 
-    public static int bfs(int N, int M, int[][] arr) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1}); // {x좌표, y좌표, 현재까지 거리}
-        boolean[][] visited = new boolean[N][M];
+    public static void bfs(){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 1});
         visited[0][0] = true;
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int x = now[0], y = now[1], dist = now[2];
-
-            // 도착지점 도달하면 최단 거리 반환
-            if (x == N - 1 && y == M - 1) {
-                return dist;
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            if (now[0] == N - 1 && now[1] == M - 1) {
+                System.out.println(now[2]);
+                return;
             }
-
-            // 4방향 탐색
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                    if (arr[nx][ny] == 1 && !visited[nx][ny]) {
-                        queue.add(new int[]{nx, ny, dist + 1});
-                        visited[nx][ny] = true; // 방문 표시
-                    }
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                int cnt = now[2] + 1;
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M && arr[nx][ny] == 1 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny, cnt});
                 }
             }
         }
-
-        return -1; // 도달 불가능한 경우
     }
 }
