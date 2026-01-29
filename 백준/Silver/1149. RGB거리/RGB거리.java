e3fdbab4400d6c92f74c2fa0ed2800705d4a9cde
welikeWatermelon <bill19985500@gmail.com>
2026-01-29 11:09:44 +0900

    [Silver I] Title: RGB거리, Time: 248 ms, Memory: 21628 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] cost = new int[N][3];
+        int[][] dp = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            cost[i][0] = sc.nextInt(); // Red
+            cost[i][1] = sc.nextInt(); // Green
+            cost[i][2] = sc.nextInt(); // Blue
+        }
+
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        }
+
+        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
+    }
+}
