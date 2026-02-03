eacb6cd1f41904113efb8c22aec227b46a184366
welikeWatermelon <bill19985500@gmail.com>
2026-02-03 10:06:46 +0900

    [Gold V] Title: 내려가기, Time: 304 ms, Memory: 42148 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -3,47 +3,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        // 전의 숫자들의 합이 윗칸에 있으면, 그 아래 칸은 자동으로 채울 수 있음
 
-        int[] minDp = new int[3];
-        int[] maxDp = new int[3];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] maxSum = new int[3];
+        int[] minSum = new int[3];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int x3 = Integer.parseInt(st.nextToken());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
 
             if (i == 0) {
-                minDp[0] = maxDp[0] = x1;
-                minDp[1] = maxDp[1] = x2;
-                minDp[2] = maxDp[2] = x3;
-
-            } else {
-                int prevMax0 = maxDp[0];
-                int prevMax1 = maxDp[1];
-                int prevMax2 = maxDp[2];
-
-                int prevMin0 = minDp[0];
-                int prevMin1 = minDp[1];
-                int prevMin2 = minDp[2];
-
-                
-                maxDp[0] = Math.max(prevMax0, prevMax1) + x1;
-                maxDp[1] = Math.max(prevMax0, Math.max(prevMax1,prevMax2)) + x2;
-                maxDp[2] = Math.max(prevMax1, prevMax2) + x3;
-
-
-                minDp[0] = Math.min(prevMin0, prevMin1) + x1;
-                minDp[1] = Math.min(prevMin0, Math.min(prevMin1,prevMin2)) + x2;
-                minDp[2] = Math.min(prevMin1, prevMin2) + x3;
+                maxSum[0] = x;
+                minSum[0] = x;
+                maxSum[1] = y;
+                minSum[1] = y;
+                maxSum[2] = z;
+                minSum[2] = z;
+                continue;
             }
-            
+
+            int beforeMaxSumX = maxSum[0];
+            int beforeMaxSumY = maxSum[1];
+            int beforeMaxSumZ = maxSum[2];
+
+            int beforeMinSumX = minSum[0];
+            int beforeMinSumY = minSum[1];
+            int beforeMinSumZ = minSum[2];
+
+            maxSum[0] = Math.max(beforeMaxSumX, beforeMaxSumY) + x;
+            maxSum[1] = Math.max(Math.max(beforeMaxSumX, beforeMaxSumY),beforeMaxSumZ) + y;
+            maxSum[2] = Math.max(beforeMaxSumY, beforeMaxSumZ) + z;
+
+            minSum[0] = Math.min(beforeMinSumX, beforeMinSumY) + x;
+            minSum[1] = Math.min(Math.min(beforeMinSumX, beforeMinSumY),beforeMinSumZ) + y;
+            minSum[2] = Math.min(beforeMinSumY, beforeMinSumZ) + z;
         }
 
-        System.out.println(Math.max(Math.max(maxDp[0],maxDp[1]),maxDp[2])+" "+Math.min(Math.min(minDp[0],minDp[1]),minDp[2]));
+        System.out.println(Math.max(Math.max(maxSum[0], maxSum[1]), maxSum[2]) + " " + Math.min(Math.min(minSum[0], minSum[1]), minSum[2]));
     }
 }
