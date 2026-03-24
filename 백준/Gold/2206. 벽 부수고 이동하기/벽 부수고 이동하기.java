5e746c49486c39ba636020a8f3529003567d1bd3
welikeWatermelon <bill19985500@gmail.com>
2026-03-24 18:32:30 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 848 ms, Memory: 117284 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,69 +1,81 @@
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
-
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static boolean[][][] visited; // [x][y][벽을 부쉈는지 여부]
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        map = new int[N][M];
-        visited = new boolean[N][M][2]; // 0: 안 부숨, 1: 부쉈음
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+
 
         for (int i = 0; i < N; i++) {
-            String line = sc.next();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
                 map[i][j] = line.charAt(j) - '0';
             }
         }
 
-        int result = bfs();
-        System.out.println(result);
+        System.out.println(findMinLoad(N, M, map));
+        // BFS
+        // 전의 상태를 가져와서 다음에 넘겨야함
+        // 만약 벽을 깼다면 1, 안깬 상태라면 0
+        // 이 때, 벽을 깬 경우는 그대로 넘기고, 꺠지 않은 경우라면 분기처리 해줘야함
+        //      벽을 만났을 때, 깰 것인가 그냥 둘 것인가
+        // 이 과정만 해주면 됨
+        // ArrayDeque를 사용해줄것
+        // visited 처리를 해줄 것 (원본 데이터는 바뀌면 안됨. 그리고 처음 도착한게 최소 일 것이기 때문에 나중에 바꿀 걱정 X - DFS는 이걸 조심해줘야함)
     }
 
-    public static int bfs() {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0, 1, 0}); // x, y, 이동횟수, 벽부순횟수
+    public static int findMinLoad(int N, int M, int[][] map) {
+        Queue<int[]> moveList = new ArrayDeque<>();
+        moveList.add(new int[]{0, 0, 0, 1});
+        boolean[][][] visited = new boolean[N][M][2];
         visited[0][0][0] = true;
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1], dist = cur[2], broken = cur[3];
+        while (!moveList.isEmpty()) {
+            int[] nowPlace = moveList.poll();
+            int x = nowPlace[0];
+            int y = nowPlace[1];
+            int crack = nowPlace[2];
+            int dist = nowPlace[3];
 
             if (x == N - 1 && y == M - 1) {
                 return dist;
             }
 
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-
-                // 벽이 아니고 방문 안 했을 때
-                // 벽 만나면 알아서 돌아감
-                if (map[nx][ny] == 0 && !visited[nx][ny][broken]) {
-                    visited[nx][ny][broken] = true;
-                    q.offer(new int[]{nx, ny, dist + 1, broken});
-                }
-
-                // 벽인데 아직 안 부쉈을 때
-                // 벽을 부쉈을때랑, 부시지 않았을 때를 다르게 생각하고 행동
-                if (map[nx][ny] == 1 && broken == 0 && !visited[nx][ny][1]) {
-                    visited[nx][ny][1] = true;
-                    q.offer(new int[]{nx, ny, dist + 1, 1});
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+                if (crack == 1) {
+                    // 벽을 꺤 상태고, 벽이 없는 곳만 다님
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny][1] && map[nx][ny] == 0) {
+                        moveList.add(new int[]{nx, ny, 1, dist + 1});
+                        visited[nx][ny][1] = true;
+                    }
+                } else { // 벽돌을 안 깬 상태고, 아무곳이나 갈 수 있음
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny][0]) {
+                        // 벽 없는 곳
+                        if (map[nx][ny] == 0) {
+                            moveList.add(new int[]{nx, ny, 0, dist + 1});
+                            visited[nx][ny][0] = true;
+                        } else { // 벽 있는 곳 (뚫는 거)
+                            moveList.add(new int[]{nx, ny, 1, dist + 1});
+                            visited[nx][ny][1] = true;
+                        }
+                    }
                 }
             }
         }
-
         return -1;
     }
 }
