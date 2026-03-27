4179d78c369865238f26580b9fb98e1ab8d68d4a
welikeWatermelon <bill19985500@gmail.com>
2026-03-28 00:29:32 +0900

    [Silver III] Title: 1로 만들기, Time: 184 ms, Memory: 21600 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -5,42 +7,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
 
+        int[] dp = new int[X + 1];
 
+        dp[1] = 0;
 
-        // 1,2,3번중 어떤 거를 써야하는지 순서가 중요한 문제임
-        // 최소의 경우의 수를 필요로 함
-        // dp 배열에 저장하면서 최솟값을 찾아야겠지?
-        // ㄱ
+        for (int i = 2; i <= X; i++) {
+            dp[i] = dp[i - 1] + 1;
 
-        int[] dp = new int[X+1];
-        dp[X] = 0;
-        for (int i = 0; i < X; i++) {
-            dp[i] = 1000000;
-        }
-
-        for (int x=X; x>=1; x--) {
-            if (x % 3 == 0) { // 1번
-                dp[x/3]= Math.min(dp[x/3],dp[x]+1); //   3으로 나누지 않고, 다른방법으로 갔을 때 / 3으로 나눴을 때
-                // 9 -> 3 으로 갈 때, 9->8->4->3 / 9->3   ---> 4 / 2 둘 중 최솟값으로 갱신
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
 
-            if (x % 2 == 0) { // 2번
-                dp[x/2] = Math.min(dp[x/2],dp[x]+1);
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
-
-            dp[x - 1] = Math.min(dp[x - 1], dp[x] + 1); // 3번
         }
 
-        System.out.println(dp[1]);
+        System.out.println(dp[X]);
+        
     }
 }
-
-
-
-// X가 3으로 나누어 떨어지면, 3으로 나눈다.
-// X가 2로 나누어 떨어지면, 2로 나눈다.
-// 1을 뺀다.
-
-
-// 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다.
-// 연산을 사용하는 횟수의 최솟값을 출력하시오.
