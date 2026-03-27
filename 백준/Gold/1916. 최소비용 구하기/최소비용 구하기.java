e6b317800bbe25dd2c86786d77c0254509b72e65
welikeWatermelon <bill19985500@gmail.com>
2026-03-27 18:18:41 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 51064 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,80 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Node implements Comparable<Node> {
-        int to;
-        int weight;
+    public static int INF = Integer.MAX_VALUE;
+    public static ArrayList<Node>[] graph;
+    public static int[] dist;
 
-        public Node(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
+    public static class Node implements Comparable<Node> {
+        int idx;
+        int cost;
+
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
         }
 
         @Override
         public int compareTo(Node o) {
-            return Integer.compare(this.weight, o.weight);
+            return this.cost - o.cost;
         }
     }
 
-    public static int N, M;
-    public static List<Node>[] graph;
-    static int[] dist;
-    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
         graph = new ArrayList[N + 1];
         dist = new int[N + 1];
+        Arrays.fill(dist, INF);
 
-        for (int i = 1; i <= N; i++) {
+        // graph 초기화
+        for (int i = 0; i <= N; i++) {
             graph[i] = new ArrayList<>();
-            dist[i] = INF;  // INF로 초기화
         }
 
         for (int i = 0; i < M; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
             graph[u].add(new Node(v, w));
         }
 
-        int start = sc.nextInt();
-        int end =sc.nextInt();
-        dijkstra(start);
-        System.out.println(dist[end]);
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        // 거리 출력
-//        for (int i = 1; i <= N; i++) {
-//            if (dist[i] == INF) {
-//                System.out.println("INF");
-//            } else {
-//                System.out.println(dist[i]);
-//            }
-//        }
+        dijk(A);
+
+        System.out.println(dist[B]);
     }
 
-
-    public static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    public static void dijk(int start) {
         dist[start] = 0;
-        pq.add(new Node(start, 0)); // 자기자신
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
 
         while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            int now = cur.to;
+            Node nowNode = pq.poll();
+            int curIdx = nowNode.idx;
+            int curCost = nowNode.cost;
 
-            if(cur.weight > dist[now]) continue;
+            if (dist[curIdx] < curCost) {
+                continue;
+            }
 
-            for (Node next : graph[now]) {
-                if (dist[next.to] > dist[now] + next.weight) {
-                    dist[next.to] = dist[now] + next.weight;
-                    pq.add(new Node(next.to, dist[next.to]));
+            for (Node next : graph[curIdx]) {
+                int nextIdx = next.idx;
+                int nextCost = next.cost;
+
+                int newCost = curCost + nextCost;
+
+                if (dist[nextIdx] > newCost) {
+                    dist[nextIdx] = newCost;
+                    pq.add(new Node(nextIdx, newCost));
                 }
             }
         }
