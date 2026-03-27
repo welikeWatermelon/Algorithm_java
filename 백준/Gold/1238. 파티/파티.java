e88a31a922085e6fc88dcebee3a6b35b57d76d16
welikeWatermelon <bill19985500@gmail.com>
2026-03-27 19:14:06 +0900

    [Gold III] Title: 파티, Time: 192 ms, Memory: 19812 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,68 +1,113 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+// 1. 파티 가는데 구함 (자신의 마을 -> X 마을)
+// 각 마을마다 다익스트라를 구해서 파티마을까지 가는데 최소 거리를 각각 넣는다 int[] goParty에다가 넣는다 ->
+// 각 마을마다 하는건 시간 에바 아님? 이건 플롣이드워셜 써야하는거 아님?
+//
+// 근데 N번 다익스트라를 해도 되거든? 근데 역방향 다익스트라 라는게 있다?
+// 순방향 그래프를 반대로 생각해서 한점에 모이는 경우를 반대로 한점에서 시작하는 경우로 생각하는거야
+// 즉, partyVillage 에서 각 N의 마을로 가는 최소 경우를 구하는거지
+
+// 2. 파티에서 오는데 구함 (X마을 -> 자신의 마을)
+// 파티마을에서 각 집까지 오는 다익스트라를 구한다 int[] goHome에다가 넣는다 -> 이건 다익스트라 한번이면 완성됨
+// 3. goHome과 goParty를 각각 더한다
+// 4. 가장 큰 값 출력
 public class Main {
-    static final int INF = 1000000000; // 무한을 의미하는 값
-    static int N, M; // N: 정점 수, M: 간선 수
-    static int[][] dist;
+    public static class Node implements Comparable<Node>{
+        int idx, cost;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 정점
-        int M = sc.nextInt(); // 간선
-        int X = sc.nextInt(); // 목적지
-
-        dist = new int[N + 1][N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
         }
 
-        // 간선 정보 입력
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static int N,M,X;
+    public static int INF = Integer.MAX_VALUE;
+    public static List<Node>[] graph;
+    public static List<Node>[] againstGraph;
+    public static int[] dist, againstDist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1];
+        againstDist = new int[N + 1];
+        graph = new ArrayList[N+1];
+        againstGraph = new ArrayList[N+1];
+
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+            againstGraph[i] = new ArrayList<>();
+        }
+
+        Arrays.fill(dist, INF);
+        Arrays.fill(againstDist, INF);
+
         for (int i = 0; i < M; i++) {
-            int u = sc.nextInt(); // 출발
-            int v = sc.nextInt(); // 도착
-            int w = sc.nextInt(); // 가중치
-            dist[u][v] = Math.min(dist[u][v], w); // 중복 간선 처리
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            graph[u].add(new Node(v, w));
+            againstGraph[v].add(new Node(u, w));
         }
 
-        // 플로이드-워셜 핵심 로직
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
-        }
+        dijk(X, dist, graph);
+        dijk(X, againstDist, againstGraph);
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= N; i++) {
-            if (i == X) {
-                continue;
+        int max = 0;
+        for (int i = 0; i <= N; i++) {
+            int sumDist = dist[i] + againstDist[i];
+            if (max < sumDist) {
+                max = sumDist;
             }
-            int sum = 0;
-            sum += dist[i][X];
-            sum += dist[X][i];
-            max = Math.max(sum, max);
         }
 
         System.out.println(max);
     }
+
+    public static void dijk(int start, int[] dist, List<Node>[] graph) {
+        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            int curIdx = curNode.idx;
+            int curCost = curNode.cost;
+
+            if (dist[curIdx] < curCost) {
+                continue;
+            }
+
+            for (Node node : graph[curIdx]) {
+                int nextIdx = node.idx;
+                int nextCost = node.cost;
+
+                int newCost = curCost + nextCost;
+
+                if (dist[nextIdx] > newCost) {
+                    dist[nextIdx] = newCost;
+                    pq.add(new Node(nextIdx, newCost));
+                }
+            }
+        }
+    }
 }
-
-
-// 각각의 학생들은 파티에 참석하기 위해 걸어가서 다시 그들의 마을로 돌아와야 한다.
-// 하지만 이 학생들은 워낙 게을러서 최단 시간에 오고 가기를 원한다.
-
-// 이 도로들은 단방향이기 때문에 아마 그들이 오고 가는 길이 다를지도 모른다.
-// N명의 학생들 중 오고 가는데 가장 많은 시간을 소비하는 학생은 누구일지 구하여라.
-
-// X에서 각 마을까지 걸리는 거리를 다익스트라로 구하여 해당 List에 넣어주고
-
-// 각 마을에서 X까지 오는데 걸리는 최소 거리를 다익스트라로 구하여 List에 넣어줌
-
-// 이럴바엔 그냥 다익스트라로 하는게 아니라 벨만포드로 하는게 낫지않나??
-
