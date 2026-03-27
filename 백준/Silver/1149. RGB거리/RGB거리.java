929e21a13439baea508887c83615e5937201d68a
welikeWatermelon <bill19985500@gmail.com>
2026-03-28 03:53:34 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,29 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+// N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+// i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
+// i번 째 집은 전에꺼랑만 같지 않으면 됨 (i+1은 생각하지말자)
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int[][] cost = new int[N][3];
-        int[][] dp = new int[N][3];
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            cost[i][0] = sc.nextInt(); // Red
-            cost[i][1] = sc.nextInt(); // Green
-            cost[i][2] = sc.nextInt(); // Blue
+        int[][] cost = new int[N + 1][3];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            int G = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            cost[i][0] = R;
+            cost[i][1] = G;
+            cost[i][2] = B;
         }
 
-        dp[0][0] = cost[0][0];
-        dp[0][1] = cost[0][1];
-        dp[0][2] = cost[0][2];
+        int[][] dp = new int[N + 1][3];
+        // [][0] : R, 1:G, 2:B
 
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-            dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-            dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        dp[1][0] = cost[1][0];
+        dp[1][1] = cost[1][1];
+        dp[1][2] = cost[1][2];
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
         }
 
-        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
+        System.out.println(Math.min(Math.min(dp[N][0],dp[N][1]),dp[N][2]));
+
+
     }
 }
