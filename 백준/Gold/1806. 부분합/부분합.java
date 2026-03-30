1a83fdae4c73f69ee3c5df1282d03d08bc45aebc
welikeWatermelon <bill19985500@gmail.com>
2026-03-30 17:44:28 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 24404 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,29 +1,40 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// s 이상 되는 것 중 가장 짧은 것을 구하라
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 수열 길이
-        int S = sc.nextInt(); // 목표 합
-        int[] arr = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int minLen = Integer.MAX_VALUE;
-        int sum = 0;
-        int start = 0;
-
-        for (int end = 0; end < N; end++) {
-            sum += arr[end];
-
-            while (sum >= S) {
-                minLen = Math.min(minLen, end - start + 1);
-                sum -= arr[start++];
+        int i = 0, j = 0, sum = 0, minCnt = Integer.MAX_VALUE;
+        while (i<=j) {
+            if (s > sum) {
+                if (j == n) {
+                    break;
+                }
+                sum += arr[j];
+                j++;
+            } else {
+                minCnt = Math.min(j-i, minCnt);
+                sum -= arr[i];
+                i++;
             }
         }
 
-        System.out.println(minLen == Integer.MAX_VALUE ? 0 : minLen);
+        System.out.println(minCnt==Integer.MAX_VALUE ? 0 : minCnt);
+
     }
 }
