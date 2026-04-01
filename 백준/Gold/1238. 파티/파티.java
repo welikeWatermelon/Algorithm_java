15ea8d77c73d9cafa22200fe8178fa9b11d31b45
welikeWatermelon <bill19985500@gmail.com>
2026-04-01 18:46:14 +0900

    [Gold III] Title: 파티, Time: 160 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,29 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-// 1. 파티 가는데 구함 (자신의 마을 -> X 마을)
-// 각 마을마다 다익스트라를 구해서 파티마을까지 가는데 최소 거리를 각각 넣는다 int[] goParty에다가 넣는다 ->
-// 각 마을마다 하는건 시간 에바 아님? 이건 플롣이드워셜 써야하는거 아님?
-//
-// 근데 N번 다익스트라를 해도 되거든? 근데 역방향 다익스트라 라는게 있다?
-// 순방향 그래프를 반대로 생각해서 한점에 모이는 경우를 반대로 한점에서 시작하는 경우로 생각하는거야
-// 즉, partyVillage 에서 각 N의 마을로 가는 최소 경우를 구하는거지
-
-// 2. 파티에서 오는데 구함 (X마을 -> 자신의 마을)
-// 파티마을에서 각 집까지 오는 다익스트라를 구한다 int[] goHome에다가 넣는다 -> 이건 다익스트라 한번이면 완성됨
-// 3. goHome과 goParty를 각각 더한다
-// 4. 가장 큰 값 출력
 public class Main {
+    // X마을에서 각 집으로 가는 것 : 그래프 방향을 순방향으로, X에서 다익스트라 graphGoToHome
+    // 각 집에서 X마을까지 가는 것 : 그래프 방향을 반대로 돌리고, X에서 다익스트라 graphGoToX
     public static class Node implements Comparable<Node>{
-        int idx, cost;
+        int idx;
+        int cost;
 
         public Node(int idx, int cost) {
             this.idx = idx;
             this.cost = cost;
         }
-
 
         @Override
         public int compareTo(Node o) {
@@ -31,11 +24,12 @@ public class Main {
         }
     }
 
+    public static ArrayList<Node>[] graphGoToHome;
+    public static ArrayList<Node>[] graphGoToX;
+    public static int[] distGoToHome;
+    public static int[] distGoToX;
     public static int N,M,X;
     public static int INF = Integer.MAX_VALUE;
-    public static List<Node>[] graph;
-    public static List<Node>[] againstGraph;
-    public static int[] dist, againstDist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,66 +40,62 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
-        dist = new int[N + 1];
-        againstDist = new int[N + 1];
-        graph = new ArrayList[N+1];
-        againstGraph = new ArrayList[N+1];
+        graphGoToHome = new ArrayList[N + 1];
+        graphGoToX = new ArrayList[N + 1];
 
-        for (int i = 0; i < N + 1; i++) {
-            graph[i] = new ArrayList<>();
-            againstGraph[i] = new ArrayList<>();
+
+        for (int i = 0; i <= N; i++) {
+            graphGoToHome[i] = new ArrayList<>();
+            graphGoToX[i] = new ArrayList<>();
         }
-
-        Arrays.fill(dist, INF);
-        Arrays.fill(againstDist, INF);
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph[u].add(new Node(v, w));
-            againstGraph[v].add(new Node(u, w));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graphGoToHome[start].add(new Node(end, cost));
+            graphGoToX[end].add(new Node(start, cost));
         }
 
-        dijk(X, dist, graph);
-        dijk(X, againstDist, againstGraph);
+        distGoToHome = new int[N + 1];
+        distGoToX = new int[N + 1];
+        Arrays.fill(distGoToHome, INF);
+        Arrays.fill(distGoToX, INF);
+
+        distGoToHome[X] = 0;
+        distGoToX[X] = 0;
+
+        dijk(X, distGoToHome, graphGoToHome);
+        dijk(X, distGoToX, graphGoToX);
 
         int max = 0;
-        for (int i = 0; i <= N; i++) {
-            int sumDist = dist[i] + againstDist[i];
-            if (max < sumDist) {
-                max = sumDist;
-            }
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(distGoToHome[i] + distGoToX[i], max);
         }
 
         System.out.println(max);
-    }
 
-    public static void dijk(int start, int[] dist, List<Node>[] graph) {
-        dist[start] = 0;
+    }
+    public static void dijk(int start, int[] dist, ArrayList<Node>[] graph) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(new Node(start, 0));
 
         while (!pq.isEmpty()) {
             Node curNode = pq.poll();
-            int curIdx = curNode.idx;
-            int curCost = curNode.cost;
+            int idx = curNode.idx;
+            int cost = curNode.cost;
 
-            if (dist[curIdx] < curCost) {
+            if (dist[idx] < cost) {
                 continue;
             }
 
-            for (Node node : graph[curIdx]) {
-                int nextIdx = node.idx;
-                int nextCost = node.cost;
-
-                int newCost = curCost + nextCost;
-
-                if (dist[nextIdx] > newCost) {
-                    dist[nextIdx] = newCost;
-                    pq.add(new Node(nextIdx, newCost));
+            for (Node nextNode : graph[idx]) {
+                int nextIdx = nextNode.idx;
+                int nextCost = nextNode.cost;
+                if (dist[nextIdx] > cost + nextCost) {
+                    dist[nextIdx] = cost + nextCost;
+                    pq.add(new Node(nextIdx, cost+nextCost));
                 }
             }
         }
