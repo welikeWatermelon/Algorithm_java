c4fe4411a85d77aa3a93014bb3f14ffd9166001b
welikeWatermelon <bill19985500@gmail.com>
2026-04-05 03:18:03 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 17.66 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 90 MB, 시간: 5.77 ms
+메모리: 86.9 MB, 시간: 17.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 04일 00:37:44
+2026년 04월 05일 03:17:56
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,83 +1,72 @@
 import java.util.*;
 
+// 0 비어있음
+// 1 벽
+// 중간에 끊기지 않도록 경주로를 건설
+// 직선도로 100
+// 코너 500
+// 
 class Node implements Comparable<Node>{
     int x;
     int y;
     int cost;
     int dir;
     public Node(int x, int y, int cost, int dir){
-        this.x = x;
-        this.y = y;
-        this.cost = cost;
-        this.dir = dir;
+        this.x = x; this. y = y; this.cost = cost; this.dir = dir;
     }
-    
     @Override
     public int compareTo(Node o){
         return this.cost - o.cost;
     }
 }
-
 class Solution {
-    public static int[] dx = {-1,1,0,0};
-    public static int[] dy = {0,0,-1,1};
-    public static int[][][] dist;
-    
+    public int[] dx = {-1,1,0,0};
+    public int[] dy = {0,0,-1,1};
+    public int finalAnswer = 0;
     public int solution(int[][] board) {
-        int answer = 0;
         int n = board.length;
-        bfs(0,0,board,n);
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<=4; i++){
-            min = Math.min(dist[n-1][n-1][i], min);
-        }
-        
-        return min;
+        boolean[][][] visited = new boolean[n][n][4];
+        bfs(n,board, visited);
+        return finalAnswer;
     }
     
-    public void bfs(int startX, int startY, int[][] board, int n){
-        dist = new int[n][n][5];
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n ;j++){
-                for(int k=0; k<5; k++){
-                    dist[i][j][k] = Integer.MAX_VALUE;
-                }
-            }
-        }
-        
-        dist[startX][startY][0] = 0;
+    public void bfs(int n, int[][] board, boolean[][][] visited){
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(startX, startY, 0, 0));
-        
+        pq.add(new Node(0,0,0,-1));
+        for(int i=0; i<4; i++){
+            visited[0][0][i] = true;
+        }
         while(!pq.isEmpty()){
-            Node curNode = pq.poll();
-            int x = curNode.x;
-            int y = curNode.y;
-            int cost = curNode.cost;
-            int dir = curNode.dir;
+            Node cur = pq.poll();
+            int curX = cur.x;
+            int curY = cur.y;
+            int curCost = cur.cost;
+            int curDir = cur.dir;
+            if(curDir!=-1){
+                visited[curX][curY][curDir] = true;
+            }
             
-            if(dist[x][y][dir] < cost) continue;
+            // 기저조건
+            if(curX==n-1 && curY==n-1){
+                finalAnswer = curCost;
+                return;
+            }
             
             for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx < 0 || ny < 0 || nx>=n || ny>=n) continue;
-                
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
+                int nCost = curCost + 100;
+                if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
                 if(board[nx][ny]==1) continue;
+                if(visited[nx][ny][i]) continue;
                 
-                int nextDir = i+1; // 1,2,3,4 -> 상,하,좌,우
-                int nextCost = 100; // 일단 한번은 가니 100원을 더해줌
-                
-                if(dir!=0 && dir!=nextDir) nextCost += 500;
-                
-                if(dist[nx][ny][nextDir]>dist[x][y][dir] + nextCost){
-                    dist[nx][ny][nextDir]=dist[x][y][dir] + nextCost;
-                    pq.add(new Node(nx,ny,dist[nx][ny][nextDir],nextDir));
+                if(curDir == -1 || curDir==i){ // 직진
+                    pq.add(new Node(nx,ny,nCost,i));
+                }else{ //방향 바꾸기 . 코너 생성
+                    nCost += 500;
+                    pq.add(new Node(nx,ny,nCost,i));
                 }
             }
         }
-        
-        
     }
 }
